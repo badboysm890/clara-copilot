b2fb367ac141d5d338dae23df83383472a77499c
BadBoy17G <praveensm890@gmail.com>
2021-07-04 21:50:59 +0530

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+
+All notable changes to the "bb-copilot" extension will be documented in this file.
+Thanks for Grepper !!
+
+## [1.0] - 4-1-21
+
+- Initial release as alternative for github pilot
+- Can search Snippets 
+- throws error upon failure
---
+++ extension.js
@@ -0,0 +1,50 @@
+const vscode = require('vscode');
+const superagent = require("superagent")
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+
+    const editor = vscode.window.activeTextEditor;
+
+    async function findSnippets(data, selections) {
+        const forumURL = "https://www.codegrepper.com/api/search.php?q=" + data + "&search_options=search_titles";
+        // callbacks
+        superagent
+            .get(forumURL)
+            .end((error, response) => {
+                let data = JSON.parse(response.text);
+                console.log(data)
+                if (data["answers"].length == 0) {
+                    vscode.window.showWarningMessage("Search with words that are simple !!")
+                }
+                editor.edit(editBuilder => {
+                    editBuilder.replace(selections, String(data["answers"][0]["answer"]));
+                });
+            })
+    }
+
+    let disposable = vscode.commands.registerCommand('clara-copilot.searchcode', async function() {
+
+        if (editor) {
+            const document = editor.document;
+            const selection = editor.selection;
+
+            const word = document.getText(selection);
+            await findSnippets(word, selection);
+
+        }
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
